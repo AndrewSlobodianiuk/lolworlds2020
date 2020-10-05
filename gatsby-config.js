96104b55a9c76b5840bba85c0ab8132ce7aab26e
Andrew Slobodianiuk <speshmillo@gmail.com>
2020-10-05 14:33:29 +0300

    add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,4 @@
 module.exports = {
-  siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-  },
   plugins: [
     `gatsby-plugin-netlify-cms`,
     `gatsby-plugin-react-helmet`,
@@ -25,7 +20,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     {
